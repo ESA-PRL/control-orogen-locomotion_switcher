a5a6e0b7c7717c1713de27c3717e147da1320507
Ricardo Sanchez <ricardosan@uma.es>
2017-04-28 10:47:20 +0200

    Adding files

---
+++ locomotion_switcherTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef locomotion_switcher_TYPES_HPP
+#define locomotion_switcher_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace locomotion_switcher {
+}
+
+#endif
+
