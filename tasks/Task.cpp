777170fc4f83165a58973c56a45eb4d968e2a0a3
Levin Gerdes <gerdeslevin@gmail.com>
2017-09-01 19:38:46 +0200

    Pausing movement does no longer straighten all steering drives.

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -6,15 +6,11 @@ using namespace locomotion_switcher;
 Task::Task(std::string const& name)
     : TaskBase(name)
 {
-    state = INITIAL;
-    isModeOverrideEnabled = true;
 }
 
 Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
     : TaskBase(name, engine)
 {
-    state = INITIAL;
-    isModeOverrideEnabled = true;
 }
 
 
@@ -29,6 +25,8 @@ bool Task::configureHook()
 {
     if (! TaskBase::configureHook())
         return false;
+    isModeOverrideEnabled = true;
+    state = INITIAL;
     return true;
 }
 
@@ -58,9 +56,9 @@ void Task::updateHook()
 
     int locomotionModeOverride;
     if (
-            (_motion_command.read(motion_command) == RTT::NewData)||                             // new motion command or
             (!isModeOverrideEnabled && (_locomotionMode.read(locomotionMode) == RTT::NewData))|| // new mode while autonomous or
-            (_locomotionMode_override.read(locomotionModeOverride) == RTT::NewData)              // new override input
+            (_locomotionMode_override.read(locomotionModeOverride) == RTT::NewData)||              // new override input
+            (_motion_command.read(motion_command) == RTT::NewData)                             // new motion command or
         )
     {
         if (locomotionModeOverride == -1)
@@ -71,10 +69,11 @@ void Task::updateHook()
         {
             locomotionMode = locomotionModeOverride;
             isModeOverrideEnabled = true;
+            std::cout << "locomotion mode: " << locomotionMode << std::endl;
         }
 
         //std::cout<<"SWITCHER: new command received: " << motion_command.translation << " " << motion_command.rotation << std::endl;
-        _lc_motion_command.write(motion_command);
+        //_lc_motion_command.write(motion_command);
         if ((motion_command.translation == 0) && (motion_command.rotation == 0))
         {
             if (state != INITIAL)
@@ -113,38 +112,51 @@ void Task::updateHook()
     {
         if(state == WWC)
         {
-            if(resetDepJoints)
-                _resetDepJoints.write(resetDepJoints = false);
-            if(isZeroWalking())
+            _resetDepJoints.write(resetDepJoints = false); //TODO consider removing, it's already in ELSE below
+
+            // make sure that the wheels are straight before using wheel walking
+            if(!isZeroSteering())
             {
-                //if(!resetDepJoints) _resetDepJoints.write(resetDepJoints = true);
-                if(kill_switch) _kill_switch.write(kill_switch = false);
-                _ww_joints_commands.read(ww_joints_commands);
-                _joints_commands.write(ww_joints_commands);
+                std::cout << "zeroing steering motors " << locomotionMode << std::endl;
+                _joints_commands.write(rectifySteering());
             }
             else
             {
+                if(kill_switch)
+                    _kill_switch.write(kill_switch = false);
+
+                std::cout << "trying to move " << locomotionMode << std::endl;
                 _ww_joints_commands.read(ww_joints_commands);
                 _joints_commands.write(ww_joints_commands);
             }
         }
         else
         {
-            if(isZeroWalking())
+            if(!kill_switch)
+                _kill_switch.write(kill_switch = true); //TODO consider removing
+
+            if(!isZeroWalking())
             {
-                if(resetDepJoints) _resetDepJoints.write(resetDepJoints = false);
-                if(!kill_switch) _kill_switch.write(kill_switch = true);
+                if(!resetDepJoints)
+                    _resetDepJoints.write(resetDepJoints = true);
+                if(_ww_joints_commands.read(ww_joints_commands) == RTT::NewData)
+                    _joints_commands.write(ww_joints_commands);
+            }
+            else
+            {
+                if(resetDepJoints)
+                    _resetDepJoints.write(resetDepJoints = false); //TODO consider removing
                 if(state == LC)
                 {
-                    if(isZeroSpeeds())
+                    //if(isZeroSpeeds()) //TODO really necessary?
                         _lc_motion_command.write(motion_command);
                     if(_lc_joints_commands.read(lc_joints_commands) == RTT::NewData)
                         _joints_commands.write(lc_joints_commands);
                 }
                 else
                 {
-                    if(isZeroSteering())
-                    {
+                    //if(isZeroSteering())
+                    //{
                         if(state == STOP)
                         {
                             if(isZeroSpeeds())
@@ -167,37 +179,21 @@ void Task::updateHook()
                             if(state == LC2WWC)
                                 state = WWC;
                         }
-                    }
-                    else
-                        _joints_commands.write(rectifySteering());
+                    //}
+                    //else
+                        //_joints_commands.write(rectifySteering()); //TODO move to (if state==WWC)
                 }
-            }
-            else
-            {
-                if(!resetDepJoints)
-                    _resetDepJoints.write(resetDepJoints = true);
-                if(!kill_switch)
-                    _kill_switch.write(kill_switch = true);
-                if(_ww_joints_commands.read(ww_joints_commands) == RTT::NewData)
-                {
-                    //std::cout<<"SWITCHER: RECTIFYING" << std::endl;
-                    _joints_commands.write(ww_joints_commands);
-                }
-                //_joints_commands.write(rectifyWalking());
             }
         }
     }
 }
 
 
-//__CHECK_IF_STEERING_JOINT_POSITIONS_ARE_ZERO
+//__CHECK_ IF_STEERING_JOINT_POSITIONS_ARE_ZERO
 bool Task::isZeroSteering()
 {
     _motors_readings.read(motors_readings);
-    for (unsigned int i = 0; i < 10; i++)
-	if (fabs(motors_readings[i].speed) > window)
-	    return false;
-    for (unsigned int i = 10; i < 16; i++)
+    for (unsigned int i = 6; i < 10; i++)
 	    if ((fabs(motors_readings[i].position) > 2*window)||(fabs(motors_readings[i].speed) > window))
 		return false;
     return true;
@@ -208,9 +204,6 @@ bool Task::isZeroSteering()
 bool Task::isZeroWalking()
 {
     _motors_readings.read(motors_readings);
-    /*for (unsigned int i = 0; i < 10; i++)
-	if (fabs(motors_readings[i].speed) > window)
-	    return false;*/
     for (unsigned int i = 10; i < 16; i++)
 	if ((fabs(motors_readings[i].position) > 2*window)||(fabs(motors_readings[i].speed) > window))
 	    return false;
@@ -254,13 +247,19 @@ base::commands::Joints Task::rectifySteering()
     rJoints.names[17] = "WHEEL_STEER_GROUP";
     rJoints.names[18] = "WHEEL_WALK_GROUP";
 
-    for (unsigned int i = 0; i < 6; i++)
-        rJoints[i].speed = 0;
     for (unsigned int i = 0; i < 16; i++)
-	if ((i > 5)&&(i < 10))
-	    rJoints[i].position = 0;
-	else
-	    rJoints[i].speed = 0;
+    {
+        if ((i > 5)&&(i < 10))
+        {
+            rJoints[i].position = 0;
+            rJoints[i].speed = base::NaN<double>();
+        }
+        else
+        {
+            rJoints[i].speed = 0;
+            rJoints[i].position = base::NaN<double>();
+        }
+    }
     for(uint i=16; i<19; i++)
     {
 	rJoints[i].speed = base::NaN<double>();
